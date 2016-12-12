53f60b4756b62c5d7f58ef750426336a72db8b4a
ammanin <ammanin@andrew.cmu.edu>
2016-12-12 08:03:28 -0500

    didnt work

--- app.rb
+++ app.rb
@@ -124,7 +124,7 @@ class CustomHandler < AlexaSkillsRuby::Handler
     trans_txt = (request.intent.slots["trans_txt"])
 	lang_input = (request.intent.slots["lang_input"])
 	
-	response.set_output_speech_text("#{trans_txt} in #{lang_input} is #{trans_met(trans_txt, lang_input)}")  
+	response.set_output_speech_text("#{trans_txt} in #{lang_input} is ")  
     #response.set_simple_card("title", "content")
   end
 
