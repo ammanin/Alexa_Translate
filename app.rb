4924caeddde900c4ac7649a9d52d2a97c85cd3ce
ammanin <ammanin@andrew.cmu.edu>
2016-12-12 07:30:10 -0500

    trying langauag input

--- app.rb
+++ app.rb
@@ -124,7 +124,7 @@ class CustomHandler < AlexaSkillsRuby::Handler
     trans_txt = (request.intent.slots["trans_txt"])
 	lang_input = (request.intent.slots["lang_input"])
 	
-	response.set_output_speech_text("#{trans_txt} in #{lang_input} is ")  
+	response.set_output_speech_text("#{trans_txt} in #{lang_input} is #{trans_met(trans_txt, lang_input)}")  
     #response.set_simple_card("title", "content")
   end
 
@@ -177,9 +177,10 @@ end
 private
 
 
-def trans_met transtxt
+def trans_met transtxt, langinput
   translator = BingTranslator.new(ENV["MICROSOFT_CLIENT_ID"], ENV["MICROSOFT_CLIENT_SECRET"])
-  langinput = "de"
+  langinput = langinput.downcase.strip
+  langinput = lang_list.where(lang_name: "langinput").lang_code
   translator.translate(transtxt, :from => 'en', :to => langinput)
 end
 
